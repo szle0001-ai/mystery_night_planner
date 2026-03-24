99013f1a540f7e820b887ad225d421bb37af5028
szle0 <szle0001@gmail.com>
2026-03-24 18:55:14 +0100

    New databse

--- backend/src/db/db.js
+++ backend/src/db/db.js
@@ -1,15 +1,14 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  connectionString: process.env.postgresql://neondb_owner:npg_6BmY5QvwIUpa@ep-sweet-unit-a9p8s0bu-pooler.gwc.azure.neon.tech/neondb?sslmode=require&channel_binding=require,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 pool.connect()
   .then(() => console.log("Connected to PostgreSQL"))
-  .catch(err => console.error("DB connection error:", err));
+  .catch((err) => console.error("DB connection error:", err));
 
 module.exports = pool;
